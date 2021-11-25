0415fc985f917c84bb1085255263bc497365cb6d
Steren <steren.giannini@gmail.com>
2021-11-25 07:26:14 +0000

    re-enable service worker

--- worker.js
+++ worker.js
@@ -5,8 +5,8 @@ self.addEventListener('install', function(event) {
     var indexPage = new Request('index.html');
     event.waitUntil(
       fetch(indexPage).then(function(response) {
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          console.log('[PWA Builder] Cached index page during Install '+ response.url);
+        return caches.open('sounds').then(function(cache) {
+          console.log('Cached index page during Install '+ response.url);
           return cache.put(indexPage, response);
         });
     }));
@@ -15,9 +15,9 @@ self.addEventListener('install', function(event) {
   //If any fetch fails, it will look for the request in the cache and serve it from there first
   self.addEventListener('fetch', function(event) {
     var updateCache = function(request){
-      return caches.open('pwabuilder-offline').then(function (cache) {
+      return caches.open('sounds').then(function (cache) {
         return fetch(request).then(function (response) {
-          console.log('[PWA Builder] add page to offline '+response.url)
+          console.log('add page to offline '+response.url)
           return cache.put(request, response);
         });
       });
@@ -27,12 +27,12 @@ self.addEventListener('install', function(event) {
   
     event.respondWith(
       fetch(event.request).catch(function(error) {
-        console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+        console.log( 'Network request Failed. Serving content from cache: ' + error );
   
         //Check to see if you have it in the cache
         //Return response
         //If not in the cache, then return error page
-        return caches.open('pwabuilder-offline').then(function (cache) {
+        return caches.open('sounds').then(function (cache) {
           return cache.match(event.request).then(function (matching) {
             var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
             return report
